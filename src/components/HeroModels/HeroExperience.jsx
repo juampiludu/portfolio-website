ee37c123df8817ddf58269183cfb0fd9e4dfa464
juampiludu <juanzakka@gmail.com>
2025-05-03 11:15:36 -0300

    feat: add 3d section component in Hero

---
+++ src/components/HeroModels/HeroExperience.jsx
@@ -0,0 +1,26 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+export default function HeroExperience() {
+  const isTablet = false;
+  return (
+    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+      <ambientLight intensity={0.2} color="#1a1a40" />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+
+      <OrbitControls
+        enablePan={false}
+        enableZoom={!isTablet}
+        maxDistance={20}
+        minDistance={5}
+        minPolarAngle={Math.PI / 5}
+        maxPolarAngle={Math.PI / 2}
+      />
+
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="teal" />
+      </mesh>
+    </Canvas>
+  );
+}
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
 import { words } from "../constants/index";
 
 export default function Hero() {
@@ -45,6 +46,11 @@ export default function Hero() {
           </div>
         </header>
         {/* RIGHT: 3D MODEL */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   );
