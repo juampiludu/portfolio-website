e4c28891c399fb720f264bb747c09f2017d7b7ac
juampiludu <juanzakka@gmail.com>
2025-05-03 11:38:42 -0300

    feat: add Room 3D Model component

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,8 +1,12 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+import Room from "./Room";
 
 export default function HeroExperience() {
-  const isTablet = false;
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <ambientLight intensity={0.2} color="#1a1a40" />
@@ -10,17 +14,14 @@ export default function HeroExperience() {
 
       <OrbitControls
         enablePan={false}
-        enableZoom={!isTablet}
+        enableZoom={!isTablet && !isMobile}
         maxDistance={20}
         minDistance={5}
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
       />
 
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="teal" />
-      </mesh>
+      <Room />
     </Canvas>
   );
 }
