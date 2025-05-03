c7e0611c77b14a10d6e88c78222e62cfb285c4e6
juampiludu <juanzakka@gmail.com>
2025-05-02 21:59:54 -0300

    feat: add hero text & layout

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-0">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+
+      <div className="hero-layout">
+        {/* LEFT: HERO CONTENT */}
+        {/* RIGHT: 3D MODEL */}
+      </div>
+    </section>
+  );
+}
