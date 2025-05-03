43d172b5c1ccfc72d22bcf70526e00cc476e560b
juampiludu <juanzakka@gmail.com>
2025-05-03 11:59:46 -0300

    feat: add model lights to 3d model

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,6 +2,7 @@ import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
 import Room from "./Room";
+import HeroLights from "./HeroLights";
 
 export default function HeroExperience() {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
@@ -9,8 +10,7 @@ export default function HeroExperience() {
 
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <HeroLights />
 
       <OrbitControls
         enablePan={false}
@@ -21,7 +21,13 @@ export default function HeroExperience() {
         maxPolarAngle={Math.PI / 2}
       />
 
-      <Room />
+      <group
+        scale={isMobile ? 0.7 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
+        <Room />
+      </group>
     </Canvas>
   );
 }
