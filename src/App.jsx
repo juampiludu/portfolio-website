b17bf0608dc7c01e9644c5834f8fe5de5755add2
juampiludu <juanzakka@gmail.com>
2025-05-07 01:39:00 -0300

    feat: add feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./sections/Hero";
 import Showcase from "./sections/Showcase";
 import NavBar from "./components/NavBar";
+import FeatureCards from "./sections/FeatureCards";
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <Showcase />
+      {/* FEAT: LOGO SHOWCASE */}
+      <FeatureCards />
     </>
   );
 };
