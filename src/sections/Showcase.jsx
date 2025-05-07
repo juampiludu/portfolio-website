764583fb2eaa9afd6c09a6da972e5d459e4b855e
juampiludu <juanzakka@gmail.com>
2025-05-07 01:09:24 -0300

    feat: add showcase animations

--- src/sections/Showcase.jsx
+++ src/sections/Showcase.jsx
@@ -1,10 +1,53 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Showcase() {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1.5 }
+    );
+
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=100",
+          },
+        }
+      );
+    });
+  }, []);
+
   return (
-    <div id="work" className="app-showcase">
+    <section id="work" ref={sectionRef} className="app-showcase">
       <div className="w-full">
         <div className="showcaselayout">
           {/* LEFT */}
-          <div className="first-project-wrapper">
+          <div className="first-project-wrapper" ref={project1Ref}>
             <div className="image-wrapper">
               <img src="/images/project1.png" alt="Project1" />
             </div>
@@ -22,14 +65,14 @@ export default function Showcase() {
           </div>
           {/* RIGHT */}
           <div className="project-list-wrapper overflow-hidden">
-            <div className="project">
+            <div className="project" ref={project2Ref}>
               <div className="image-wrapper bg-[#ffefdb]">
                 <img src="/images/project2.png" alt="Project2" />
               </div>
               <h2>Lorem, ipsum dolor.</h2>
             </div>
 
-            <div className="project">
+            <div className="project" ref={project3Ref}>
               <div className="image-wrapper bg-[#ffe7eb]">
                 <img src="/images/project3.png" alt="Project3" />
               </div>
@@ -38,6 +81,6 @@ export default function Showcase() {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
