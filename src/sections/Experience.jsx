1db0650653af093274e150e4b223b042a721b361
juampiludu <juanzakka@gmail.com>
2025-05-07 11:27:23 -0300

    feat: add experience cards

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -10,14 +10,42 @@ export default function Experience() {
       <div className="w-full h-full md:px-20 px-5">
         <TitleHeader
           title="Professional Work Experience"
-          sub="My Career Overview"
+          sub="💼 My Career Overview"
         />
 
         <div className="mt-32 relative">
           <div className="relative z-50 xl:space-y-32 space-y-10">
             {expCards.map((card) => (
               <div key={card.title} className="exp-card-wrapper">
-                {card.title}
+                <div className="xl:w-1/6">{/* FEAT: GLOW CARD */}</div>
+                <div className="xl:w-5/6">
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      <div className="timeline">
+                        <div className="gradient-line w-1 h-full"></div>
+                      </div>
+                    </div>
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                        <img src={card.logoPath} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-3xl">{card.title}</h1>
+                        <p className="my-5 text-white-50">🗓️ {card.date}</p>
+                        <p className="text-[#839cb5] italic">
+                          Responsabilities
+                        </p>
+                        <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
+                          {card.responsibilities.map((responsibility) => (
+                            <li key={responsibility} className="text-lg">
+                              {responsibility}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
