8e14b01497cb105b7f22b356b9e107be0499c16b
juampiludu <juanzakka@gmail.com>
2025-05-07 01:26:35 -0300

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import Showcase from "./sections/Showcase";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <Showcase />
     </>
