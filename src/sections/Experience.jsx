0e4041c9420767b460885b52cf6fc906bb5da42d
juampiludu <juanzakka@gmail.com>
2025-05-07 11:52:22 -0300

    feat: add experience scroll animation

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,7 +1,42 @@
+import gsap from "gsap";
 import TitleHeader from "../components/TitleHeader";
 import { expCards } from "../constants/index";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollToPlugin);
 
 export default function Experience() {
+  useGSAP(() => {
+    gsap.to(".timeline", {
+      transformOrigin: "bottom bottom",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: ".timeline",
+        start: "top center",
+        end: "70% center",
+        onUpdate: (self) => {
+          gsap.to(".timeline", {
+            scaleY: 1 - self.progress,
+          });
+        },
+      },
+    });
+
+    gsap.utils.toArray(".expText").forEach((text) => {
+      gsap.from(text, {
+        xPercent: 0,
+        opacity: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: text,
+          start: "top 60%",
+        },
+      });
+    });
+  }, []);
+
   return (
     <section
       id="experience"
@@ -17,13 +52,12 @@ export default function Experience() {
           <div className="relative z-50 xl:space-y-32 space-y-10">
             {expCards.map((card) => (
               <div key={card.title} className="exp-card-wrapper">
-                <div className="xl:w-1/6">{/* FEAT: GLOW CARD */}</div>
-                <div className="xl:w-5/6">
+                <div className="xl:w-2/6">{/* FEAT: GLOW CARD */}</div>
+                <div className="xl:w-4/6">
                   <div className="flex items-start">
                     <div className="timeline-wrapper">
-                      <div className="timeline">
-                        <div className="gradient-line w-1 h-full"></div>
-                      </div>
+                      <div className="timeline" />
+                      <div className="gradient-line w-1 h-full" />
                     </div>
                     <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
                       <div className="timeline-logo">
