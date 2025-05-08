19473e1a33eb5781937b784544562c0a9e7e6123
juampiludu <juanzakka@gmail.com>
2025-05-08 02:22:05 -0300

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FeatureCards from "./sections/FeatureCards";
 import Experience from "./sections/Experience";
 import TechStack from "./sections/TechStack";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -17,7 +18,9 @@ const App = () => {
       <FeatureCards />
       <Experience />
       <TechStack />
+      {/* FEAT: TESTIMONIALS */}
       <Contact />
+      <Footer />
     </React.StrictMode>
   );
 };
